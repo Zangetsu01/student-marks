96313502b7ff10bf18fc19317a661d9c3212ed6b
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-11-11 09:04:47 +0530

    updated

--- studentmarks.py
+++ studentmarks.py
@@ -22,3 +22,19 @@ average = total / 5
 
 print("Total Marks:", total)
 print("Average Marks:", average)
+
+# ----- Grade Logic -----
+if average >= 90:
+    grade = "A+"
+elif average >= 80:
+    grade = "A"
+elif average >= 70:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade:", grade)
